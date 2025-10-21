20b9ecccd4132c0a1bd6658e05a2f1e1d40bdd79
Anh Dau <anhddv98@gmail.com>
2025-10-21 20:03:02 +0700

    add ws route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import cookie from "@fastify/cookie";
 import cors from "@fastify/cors";
+import websocket from "@fastify/websocket";
 import dotenv from "dotenv";
 import fastify from "fastify";
 
 import { authRoutes } from "./modules/auth/index.js";
+import messagingRoutes from "./modules/messaging/index.js";
 
 dotenv.config({ path: "./.env" });
 
-// test server health
 const server = fastify({ logger: true });
 
 // register plugins and routes
@@ -17,7 +18,9 @@ server
     credentials: true,
   })
   .register(cookie)
-  .register(authRoutes);
+  .register(websocket)
+  .register(authRoutes)
+  .register(messagingRoutes);
 
 server.get("/", async (request, reply) => {
   reply.send("pong!\n");
@@ -28,5 +31,6 @@ server.listen({ port: 8080 }, (err, address) => {
     console.error(err);
     process.exit(1);
   }
+
   console.log(`Server listening at ${address}`);
 });
---
+++ src/modules/messaging/index.ts
@@ -0,0 +1,62 @@
+import type { WebSocket } from "@fastify/websocket";
+import type { FastifyInstance } from "fastify";
+
+type Message =
+  | {
+      type: "join";
+      roomId: string;
+    }
+  | {
+      type: "message";
+      roomId: string;
+      text: string;
+    };
+
+const rooms = new Map<string, Set<WebSocket>>();
+const COOKIE_NAME = "chatify_access_jwt";
+
+export default async function messagingRoutes(fastify: FastifyInstance) {
+  fastify.get("/ws", { websocket: true }, (socket, request) => {
+    // TODO: authenticate user here
+    console.log(`${COOKIE_NAME}=${request.cookies[COOKIE_NAME]}`);
+
+    socket.on("message", (raw) => {
+      const msg: Message = JSON.parse(raw.toString());
+
+      switch (msg.type) {
+        case "join":
+          if (!rooms.has(msg.roomId)) {
+            rooms.set(msg.roomId, new Set());
+          }
+
+          const roomToJoin = rooms.get(msg.roomId);
+          if (roomToJoin) {
+            roomToJoin.add(socket);
+            console.log(`a person has been added to the room ${msg.roomId}`);
+          }
+          break;
+
+        case "message":
+          const roomToBroadcast = rooms.get(msg.roomId);
+          if (roomToBroadcast) {
+            roomToBroadcast.forEach((client) => {
+              if (client !== socket) {
+                client.send(JSON.stringify(msg));
+                console.log(
+                  `sent "${msg.text}" to a person in the room "${msg.roomId}"`
+                );
+              }
+            });
+          }
+          break;
+      }
+    });
+
+    // Remove connection from every room upon socket close
+    socket.on("close", () => {
+      for (const room of rooms.values()) {
+        room.delete(socket);
+      }
+    });
+  });
+}
