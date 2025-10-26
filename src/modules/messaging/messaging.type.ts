27bfb3354ff7e400c385d791ec1d90507fcee74d
Anh Dau <anhddv98@gmail.com>
2025-10-26 09:03:17 +0700

    update type property comments

--- src/modules/messaging/messaging.type.ts
+++ src/modules/messaging/messaging.type.ts
@@ -5,8 +5,8 @@ interface ChatRoom {
   name?: string; // for group chats
   members: PublicUser[];
   isGroup: boolean;
-  lastMsgAt: number; // timestamp, 0 if no messages yet
-  lastMsg?: ChatMessage; // unavailble if lastMsgAt is 0
+  lastMsgAt: number; // timestamp of last message, 0 if no messages yet
+  lastMsg?: ChatMessage; // preview of last message, unavailable if lastMsgAt is 0
 }
 
 interface ChatMessage {
